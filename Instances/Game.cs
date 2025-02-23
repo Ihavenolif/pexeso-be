09618a81d315f2b70641e12f130d30d28fae282f
ihavenolif <matuswall@gmail.com>
2025-02-23 19:06:38 +0100

    task update

--- Instances/Game.cs
+++ Instances/Game.cs
@@ -49,7 +49,11 @@ class Game : Instance
         }
         else
         {
-            EvaluateTurn((int)cardRevealed, cardClickedMessage.id);
+            if (cardRevealed == cardClickedMessage.id) return;
+            Task.Run(async () =>
+            {
+                await EvaluateTurn((int)cardRevealed, cardClickedMessage.id);
+            });
         }
 
         CardClickedResponse response = new CardClickedResponse(
@@ -70,7 +74,7 @@ class Game : Instance
         }
     }
 
-    void EvaluateTurn(int cardId1, int cardId2)
+    async Task EvaluateTurn(int cardId1, int cardId2)
     {
         Character char1 = characters[cardId1]!;
         Character char2 = characters[cardId2]!;
@@ -89,14 +93,16 @@ class Game : Instance
 
         TurnEvaluationResponse response = new TurnEvaluationResponse(correctGuess, clients);
 
+        await Task.Delay(1500);
+
         foreach (Client client in clients)
         {
-            client.SendWithDelay(response, 2000);
+            client.Send(response);
         }
 
         foreach (Client client in spectators)
         {
-            client.SendWithDelay(response, 2000);
+            client.Send(response);
         }
     }
 
@@ -107,29 +113,29 @@ class Game : Instance
         int characterId = 1;
 
         list.Add(new Character("Nolifeknight", WowClass.DeathKnight, WowRole.Tank, characterId++));
-        list.Add(new Character("Zoromstitel", WowClass.Paladin, WowRole.Tank, characterId++));
-        list.Add(new Character("Lanaes", WowClass.Druid, WowRole.Tank, characterId++));
-        list.Add(new Character("DPH", WowClass.Warlock, WowRole.Tank, characterId++));
-        list.Add(new Character("Nevrmore", WowClass.Hunter, WowRole.Tank, characterId++));
-        list.Add(new Character("Mia", WowClass.Warrior, WowRole.Tank, characterId++));
-        list.Add(new Character("Littlechilla", WowClass.Shaman, WowRole.Tank, characterId++));
+        list.Add(new Character("Zoromstitel", WowClass.Paladin, WowRole.DPS, characterId++));
+        list.Add(new Character("Lanaes", WowClass.Druid, WowRole.Healer, characterId++));
+        list.Add(new Character("DPH", WowClass.Warlock, WowRole.DPS, characterId++));
+        list.Add(new Character("Nevrmore", WowClass.Hunter, WowRole.DPS, characterId++));
+        list.Add(new Character("Mia", WowClass.Warrior, WowRole.DPS, characterId++));
+        list.Add(new Character("Littlechilla", WowClass.Shaman, WowRole.Healer, characterId++));
         list.Add(new Character("Sussile", WowClass.Monk, WowRole.Tank, characterId++));
-        list.Add(new Character("Dralf", WowClass.Druid, WowRole.Tank, characterId++));
-        list.Add(new Character("Polichan", WowClass.Paladin, WowRole.Tank, characterId++));
+        list.Add(new Character("Dralf", WowClass.Druid, WowRole.DPS, characterId++));
+        list.Add(new Character("Polichan", WowClass.Paladin, WowRole.Healer, characterId++));
         list.Add(new Character("Jeffgoldblum", WowClass.Warrior, WowRole.Tank, characterId++));
-        list.Add(new Character("Rastyrose", WowClass.Paladin, WowRole.Tank, characterId++));
-        list.Add(new Character("Icansheepyou", WowClass.Mage, WowRole.Tank, characterId++));
-        list.Add(new Character("Shadowbtw", WowClass.Mage, WowRole.Tank, characterId++));
-        list.Add(new Character("Alied", WowClass.DemonHunter, WowRole.Tank, characterId++));
-        list.Add(new Character("Myrrelw", WowClass.Warlock, WowRole.Tank, characterId++));
-        list.Add(new Character("Egbanek", WowClass.Evoker, WowRole.Tank, characterId++));
+        list.Add(new Character("Rastyrose", WowClass.Paladin, WowRole.DPS, characterId++));
+        list.Add(new Character("Icansheepyou", WowClass.Mage, WowRole.DPS, characterId++));
+        list.Add(new Character("Shadowbtw", WowClass.Mage, WowRole.DPS, characterId++));
+        list.Add(new Character("Alied", WowClass.DemonHunter, WowRole.DPS, characterId++));
+        list.Add(new Character("Myrrelw", WowClass.Warlock, WowRole.DPS, characterId++));
+        list.Add(new Character("Egbanek", WowClass.Evoker, WowRole.DPS, characterId++));
         list.Add(new Character("Dádulák", WowClass.DemonHunter, WowRole.Tank, characterId++));
-        list.Add(new Character("Remath", WowClass.Shaman, WowRole.Tank, characterId++));
-        list.Add(new Character("Olgrin", WowClass.Hunter, WowRole.Tank, characterId++));
-        list.Add(new Character("Kelthrian", WowClass.DeathKnight, WowRole.Tank, characterId++));
-        list.Add(new Character("Thekropicka", WowClass.DeathKnight, WowRole.Tank, characterId++));
-        list.Add(new Character("Julka", WowClass.Hunter, WowRole.Tank, characterId++));
-        list.Add(new Character("Toluus", WowClass.Shaman, WowRole.Tank, characterId++));
+        list.Add(new Character("Remath", WowClass.Shaman, WowRole.DPS, characterId++));
+        list.Add(new Character("Olgrin", WowClass.Hunter, WowRole.DPS, characterId++));
+        list.Add(new Character("Kelthrian", WowClass.DeathKnight, WowRole.DPS, characterId++));
+        list.Add(new Character("Thekropicka", WowClass.DeathKnight, WowRole.DPS, characterId++));
+        list.Add(new Character("Julka", WowClass.Hunter, WowRole.DPS, characterId++));
+        list.Add(new Character("Toluus", WowClass.Shaman, WowRole.DPS, characterId++));
 
         return list;
     }
